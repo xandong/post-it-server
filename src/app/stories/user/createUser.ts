1b686e919474c1ccc89d1505088888015f893d1c
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-21 17:24:23 -0300

    remove const user

--- src/app/stories/user/createUser.ts
+++ src/app/stories/user/createUser.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
+import { database } from "../../../api/db/PrismaClient";
 import { UserModel } from "../../../core/models/UserModel";
 
 export async function createUser(req: Request, res: Response) {
-  const { name, email, password } = req.body;
-  const user: UserModel = { name, email, password };
+  const { name, email, password }: UserModel = req.body;
   const formatEmailValid = /\S+@\S+\.\S+/;
 
   if (!name || !email || !password)
@@ -15,13 +15,14 @@ export async function createUser(req: Request, res: Response) {
   if (password.length < 8 || password.length > 30)
     return res.status(400).json({ message: "Senha inválida" });
 
-  // try {
-  //   createUser({ name, email, password });
-  // } catch (error) {
-  //   console.log(error);
-  //   return res.status(502).json({ message: "Erro ao cadastrar" });
-  // }
+  try {
+    await database.user.create({
+      data: { name, email, password },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(502).json({ message: "Erro ao cadastrar" });
+  }
 
-  console.log(user);
   return res.status(201).json({ message: "Usuário criado com sucesso!" });
 }
