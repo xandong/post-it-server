d3a7b2704813e769dfdbc754487c435a14f8a243
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-26 08:48:38 -0300

    alter db from postgres to sqlite

---
+++ prisma/migrations/20220926114016_create_tables/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "notes" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "date" DATETIME NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "content" TEXT,
+    "authorId" TEXT NOT NULL,
+    CONSTRAINT "notes_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
