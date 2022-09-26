2a1e11c07be4d879ccb71667b656cf0d9397ecd2
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-26 20:46:08 -0300

    create get notes by user

--- src/api/routes/userRouter.ts
+++ src/api/routes/userRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createUser } from "../../useCases/user/createUser";
 import { deleteUser } from "../../useCases/user/deleteUser";
+import { getAllNotesByUser } from "../../useCases/user/getAllNotesByUser";
 import { getAllUsers } from "../../useCases/user/getAllUsers";
 import { getUserById } from "../../useCases/user/getUserByID";
 import { updateUser } from "../../useCases/user/updateUser";
@@ -8,9 +9,11 @@ import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 export const userRouter = Router();
 
-// userRouter.get("/", getAllUsers);
+userRouter.get("/", getAllUsers);
 
 userRouter.get("/:id", ensureAuthenticated, getUserById);
+
+userRouter.get("/notes/:id", getAllNotesByUser);
 
 userRouter.post("/", createUser);
 
