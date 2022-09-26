8a45e227994f04add9333d6fcfb3cb6f788b0b6a
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-26 20:45:32 -0300

    reset db

--- prisma/migrations/20220926230932_create_tables/migration.sql
+++ prisma/migrations/20220926230932_create_tables/migration.sql
@@ -14,8 +14,12 @@ CREATE TABLE "notes" (
     "description" TEXT NOT NULL,
     "content" TEXT,
     "authorId" TEXT NOT NULL,
+    "authorName" TEXT NOT NULL,
     CONSTRAINT "notes_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_id_email_key" ON "users"("id", "email");
