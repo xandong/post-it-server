566916b78f682ed44d023c8eabc186a1c1cff2d9
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-26 10:11:09 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express, { json } from "express";
+import express, { json, Request } from "express";
+import cors, { CorsOptions, CorsOptionsDelegate } from "cors";
 import { router } from "./api/routes/router";
 
 const app = express();
 const PORT = process.env.PORT || 3333;
 
+app.use(cors());
 app.use(json());
 app.use(router);
 
