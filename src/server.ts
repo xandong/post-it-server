943bf221f539589e45926c0c30f0a9a657460db2
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-28 22:52:59 -0300

    refactor(server.ts)

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,9 @@ import { router } from "./api/routes/router";
 import cors from "cors";
 
 const app = express();
-const PORT = 3333;
 
 app.use(cors());
 app.use(json());
 app.use(router);
 
-app.listen(PORT, () => console.log("Server is running..."));
+app.listen(8080, () => console.log("Server is running..."));
